95eef5573a950be9f1b09771cbccee2d01035656
Mostafa Said <mostafa.said.ext@orange.com>
2022-01-29 13:17:49 +0200

    build_scripts

--- reactnd-contacts-complete/bin/deploy.sh
+++ reactnd-contacts-complete/bin/deploy.sh
@@ -1,2 +1,2 @@
 aws s3 cp --recursive build/ s3://capstone-project-public-bucket
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./build/index.html s3://capstone-project-public-bucket
+aws s3 cp --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./build/index.html s3://capstone-project-public-bucket
