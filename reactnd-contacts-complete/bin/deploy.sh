d9c47ca33c034c4e3402acdbc2df056157156705
Mostafa Said <mostafa.said.ext@orange.com>
2022-01-29 13:04:47 +0200

    build_scripts

--- reactnd-contacts-complete/bin/deploy.sh
+++ reactnd-contacts-complete/bin/deploy.sh
@@ -1,1 +1,2 @@
-aws s3 cp --recursive --acl public-read ./build s3://udagram-test01/
+aws s3 cp --recursive build/ s3://capstone-project-public-bucket
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./build/index.html s3://capstone-project-public-bucket
